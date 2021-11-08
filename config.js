d19339582a843fb1890e54e4f8e85cdecfa7e9ba
pikpinky <93930552+pikpinky@users.noreply.github.com>
2021-11-09 00:50:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Nhọt à!',
+    introDesc: `Chào bủi đêm. Hê hê mai có đứa học sml.`,
+    btnIntro: 'Tuỵt',
+    title: 'Khai thật đi có phải cô bị ngokngheck đúng khum 😙',
+    desc: ' Hê cái đồ hay thức khuya :3  ',
+    btnYes: 'T khum hâm thì ai hâm :3',
+    btnNo: 'Có m bị hâm ế -.-',
+    question:'Chọn đúng ròi đó! Giờ mới nhận ra điều đó à?',
+    btnReply: 'ô kê lẹt gô',
+    reply: 'Hứ! Ngồi xuống đây và t sẽ kể m nghe nay t ngungok như nào.',
+    mess: 'T biết mà 😎. Trải chiếu lót dép hóng sẵn rồi nè 🤭🤭',
+    messDesc: 'Kể lun đi đợi mãi hê hê',
+    btnAccept: 'Oke go',
+    messLink: 'https://www.facebook.com/thienngoc34'
+}
